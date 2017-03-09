170b464b4e3e6f792b675fd12fad3b68c133962f
victor-mejia <victor.mejia@engrade.com>
2017-03-08 16:15:47 -0800

    header component

---
+++ src/app/header/header.component.spec.ts
@@ -0,0 +1,31 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeaderComponent } from './header.component';
+
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HeaderComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render a title', () => {
+    const elem = fixture.debugElement.nativeElement;
+
+    expect(elem.querySelector('h1.header').innerHTML).toBe('ng2 storefront');
+  });
+});
