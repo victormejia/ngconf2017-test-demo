0bc3eb9be17aef87a99ef53c6949692454886366
Victor Mejia <victor.a.mejia@gmail.com>
2017-03-24 23:28:53 -0700

    fix product component test

--- src/app/product/product.component.spec.ts
+++ src/app/product/product.component.spec.ts
@@ -20,19 +20,35 @@ describe('ProductComponent', () => {
     elem = fixture.debugElement.nativeElement;
   });
 
-  it('should render product title given an input', () => {
-
-    component.product = {
-      title: 'Angular T-Shirt',
-      price: 25.99,
-      popular: false,
-      rating: 4,
-      img: 'https://raw.githubusercontent.com/victormejia/victormejia.github.io/master/assets/images/ngshirt.png'
-    };
-
-    fixture.detectChanges();
-
-    expect(elem.querySelector('.product-title').innerHTML).toBe(component.product.title);
+  describe('addToCart', () => {
+    it('should call add on CartService', () => {
+      // ...
+    });
   });
+
+  describe('editDetails', () => {
+    it('should check permisions on UserService', () => {
+      // ...
+    });
+  });
+
+  describe('component functionality', () => {
+    it('should render product title given an input', () => {
+
+      component.product = {
+        title: 'Angular T-Shirt',
+        price: 25.99,
+        popular: false,
+        rating: 4,
+        img: 'https://raw.githubusercontent.com/victormejia/victormejia.github.io/master/assets/images/ngshirt.png'
+      };
+
+      fixture.detectChanges();
+
+      expect(elem.querySelector('.product-title').innerHTML).toBe(component.product.title);
+    });
+  });
+
+
 
 });
