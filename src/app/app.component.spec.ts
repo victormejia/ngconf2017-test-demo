bfd837d2402a916cfedd84738c4f503c6c184769
Victor Mejia <victor.a.mejia@gmail.com>
2017-04-04 11:58:09 -0700

    app component tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,19 +1,31 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, fakeAsync, tick, ComponentFixture } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ApiService } from './shared/api.service';
 import { Observable } from 'rxjs/Observable';
 import { Observer } from 'rxjs/Observer';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/delay';
+
+const mockProduct = {
+  title: 'Angular T-Shirt',
+  price: 2499,
+  popular: false,
+  rating: 4,
+  img: 'https://raw.githubusercontent.com/victormejia/victormejia.github.io/master/assets/images/ngshirt.png'
+};
 
 class MockApiService {
   getProducts() {
-    return Observable.create((observer: Observer<any>) => {
-      observer.next([]);
-    });
+    return Observable.of([mockProduct]);
   }
 }
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let elem: HTMLElement;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -25,5 +37,22 @@ describe('AppComponent', () => {
       ]
     }).compileComponents();
   }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    elem = fixture.debugElement.nativeElement;
+  });
+
+  describe('ngOnInit', () => {
+    it('should retrieve products', async(() => {
+      component.ngOnInit();
+
+      fixture.whenStable()
+        .then(() => {
+          expect(component.products).toEqual([mockProduct]);
+        });
+    }));
+  });
 
 });
